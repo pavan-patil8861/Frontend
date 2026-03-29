8e64c40187750c13e40f391d2a523f937d453991
pavan-patil8861 <pavanpatil8861@gmail.com>
2026-03-29 23:52:39 +0530

    added HTML folder

---
+++ Desktop/Front End/HTML/demo.c
@@ -0,0 +1,18 @@
+
+int main()
+{}
+    int i=0;
+    int j=1;
+
+        for(int i=0; i%2==0; i++){
+            for(int j=1; j%2==1; j++)
+            if(i%2==0){
+                    printf("1");}
+                    else{
+                    printf("0");}
+                    i=i+1;
+                    j=j+1;
+                    print("/n")
+    }
+return 0;
+}
